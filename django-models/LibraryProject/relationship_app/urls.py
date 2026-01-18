fc948fa84aec12f3285c68d68b17e7d66f84fdd4
ahmedkafro <167016211+ahmedkafro@users.noreply.github.com>
2026-01-18 21:27:37 +0000

    Add user authentication views and templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,29 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 
+#from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
+urlpatterns = [
+    # Built-in views for login and logout
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    
+    # Custom registration view
+    path('register/', views.register, name='register'),
+    
+    # Keep your previous URLs
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+]
+
+
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 <<<<<<< HEAD
 ]
 =======
-]
+
 >>>>>>> edc3c0a (add update)
