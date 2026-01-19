db8d5ccacbfa15ea306a01c35dfd552ecc2fc3a6
ahmedkafro <167016211+ahmedkafro@users.noreply.github.com>
2026-01-19 14:08:32 +0200

    Add list_books function and LibraryDetailView class

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
+#from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+
+from .models import Book, Library
+
+
+# -------------------------------
+# Function-Based View
+# -------------------------------
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'list_books.html', {'books': books})
+
+
+# -------------------------------
+# Class-Based View
+# -------------------------------
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+
+
+
+
 # Role check functions
 def is_admin(user):
     return user.is_authenticated and user.userprofile.role == 'Admin'
