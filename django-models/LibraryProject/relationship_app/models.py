5ba419dd672c92df4f69fed945aa623f3063bfdf
ahmedkafro <167016211+ahmedkafro@users.noreply.github.com>
2026-01-19 13:39:51 +0200

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,6 +2,53 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+#from django.db import models
+#from django.contrib.auth.models import User
+
+# OneToOne relationship
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+# ForeignKey relationship
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name='books'
+    )
+
+    def __str__(self):
+        return self.title
+
+
+# ManyToMany relationship
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    students = models.ManyToManyField(Student, related_name='courses')
+
+    def __str__(self):
+        return self.title
+
 
 class UserProfile(models.Model):
     ROLE_CHOICES = (
