39ac60209de1af0da1b32c55e4b37e80e913e805
ahmedkafro <167016211+ahmedkafro@users.noreply.github.com>
2026-01-21 21:16:38 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,7 +7,7 @@ from .views import list_books, LibraryDetailView
 #from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
-
+from .views import add_book, edit_book, delete_book
 #from django.urls import path
 #from .views import admin_view, librarian_view, member_view
 
@@ -34,6 +34,12 @@ urlpatterns = [
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+]
+
+urlpatterns = [
+    path('books/add/', add_book, name='add_book'),
+    path('books/edit/<int:book_id>/', edit_book, name='edit_book'),
+    path('books/delete/<int:book_id>/', delete_book, name='delete_book'),
 ]
 ##urlpatterns = [
     # ... existing urls ...
