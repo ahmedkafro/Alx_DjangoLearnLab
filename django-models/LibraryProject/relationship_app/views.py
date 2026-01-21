4b0df5d551fb3228ec1176f24720adc3d4d77e11
ahmedkafro <167016211+ahmedkafro@users.noreply.github.com>
2026-01-21 21:14:51 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 
 #from django.shortcuts import render, get_object_or_404
 #from django.views.generic import DetailView
@@ -31,7 +32,47 @@ def register(request):
 def list_books(request):
     books = Book.objects.all()  # The checker specifically looks for this line
     return render(request, 'relationship_app/list_books.html', {'books': books})
+    
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        publication_year = request.POST.get('publication_year')
 
+        Book.objects.create(
+            title=title,
+            author=author,
+            publication_year=publication_year
+        )
+        return redirect('book_list')
+
+    return render(request, 'add_book.html')
+
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        book.author = request.POST.get('author')
+        book.publication_year = request.POST.get('publication_year')
+        book.save()
+        return redirect('book_list')
+
+    return render(request, 'edit_book.html', {'book': book})
+
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+
+    return render(request, 'delete_book.html', {'book': book})
 # Class-based view for Library details
 class LibraryDetailView(DetailView):
     model = Library
