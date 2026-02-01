daa3bababb249b5326f2f4a990e69549f761f59e
ahmedkafro <167016211+ahmedkafro@users.noreply.github.com>
2026-02-01 17:53:14 +0000

    add updates for api views

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
 # Create your views here.
+from rest_framework.generics import ListAPIView
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+    
