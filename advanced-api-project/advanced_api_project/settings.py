9e00bb4777032f5d4de54d9ba88d2e81150035c0
ahmedkafro <167016211+ahmedkafro@users.noreply.github.com>
2026-02-08 21:25:27 +0200

    Add django_filters to REST framework configuration
    Added Django Filters to REST framework settings.

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
      "rest_framework",
+    "django_filters",
     "api",
 ]
 
@@ -70,6 +71,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "advanced_api_project.wsgi.application"
 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+}
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
