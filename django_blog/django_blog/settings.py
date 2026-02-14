0d5d12ea6af2391d936e2edba0d98177db269152
ahmedkafro <167016211+ahmedkafro@users.noreply.github.com>
2026-02-14 12:09:52 +0000

    add user auth updates

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -120,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+LOGIN_REDIRECT_URL = "profile"
+LOGOUT_REDIRECT_URL = "login"
+
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
