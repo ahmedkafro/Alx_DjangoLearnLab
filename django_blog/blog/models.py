8a03982a9b0dd068b621d9a0012811468f4a87bd
ahmedkafro <167016211+ahmedkafro@users.noreply.github.com>
2026-02-14 11:38:30 +0000

    add updates

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     
