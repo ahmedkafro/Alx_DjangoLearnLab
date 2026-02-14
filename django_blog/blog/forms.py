3e586ee5cf18e076fcb38443f20281018fc37822
ahmedkafro <167016211+ahmedkafro@users.noreply.github.com>
2026-02-14 16:23:27 +0000

    add updates

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post , Tag
-
+from taggit.forms import TagWidget
 from .models import Comment
 
 class RegisterForm(UserCreationForm):
@@ -34,7 +34,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content"]        
-        
+        widgets = {
+            "tags": TagWidget(),
+        }
     def save(self, commit=True):
         post = super().save(commit=False)
         if commit:
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-
+from taggit.managers import TaggableManager
 class Tag(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
@@ -14,7 +14,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     published_date = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    tags = models.ManyToManyField(Tag, related_name="posts", blank=True)
+    tags = models.ManyToManyField(Tag,TaggableManager, related_name="posts", blank=True)
     
     def __str__(self):                  
         return self.title
